95bd41cad53534a0911aa9c8c5403ee8185c55fc
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-07-17 23:45:40 -0300

    refactor: Move file App for folder src.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Routes from './src/routes';
+import Routes from './routes/index';
 import {NavigationContainer} from '@react-navigation/native';
-import {AuthProvider} from './src/contexts/AuthContext';
+import {AuthProvider} from './contexts/AuthContext';
 
 const App = () => {
   return (
