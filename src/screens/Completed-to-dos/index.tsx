8e555beffc804d5f0e8727bf38c15d6f4c6f0b45
pedroviniciusn <pedrovinicius.dev@gmail.com>
2024-08-01 21:27:19 -0300

    feat: Adds Tab bar navigation.

---
+++ src/screens/Completed-to-dos/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Text} from 'react-native';
+import {SafeAreaView} from 'react-native';
+
+const CompletedToDos = () => {
+  return (
+    <SafeAreaView>
+      <Text>Completed To-Dos</Text>
+    </SafeAreaView>
+  );
+};
+
+export default CompletedToDos;
